22839512bf6d02f66a28c314bd3bba588dcc81f0
dapihelg <xlx-1@hotmail.com>
2023-04-03 10:22:08 -0600

    1ra Update

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> pila1 = new Stack<>();
+
+        pila1.push(30);
+        pila1.push(50);
+        pila1.push(70);
+        pila1.push(90);
+        pila1.push(999);
+
+        Stack<Integer> pila2 = new Stack<>();
+
+        System.out.println("Pila 1 ANTES de sacar los enteros "+pila1);
+        System.out.println("Pila 2 ANTES de introducir los enteros "+pila2);
+        moverEnteros(pila1, pila2);
+        System.out.println("Pila 1 DESPUÉS de sacar los enteros "+pila1);
+        System.out.println("Pila 2 DESPUÉS de introducir los enteros "+pila2);
+    }
+
+    public static void moverEnteros(Stack<Integer> pila1, Stack<Integer> pila2) {
+        while (!pila1.empty()) {
+            int entero = pila1.pop();
+            pila2.push(entero);
+        }
+    }
+}
+
